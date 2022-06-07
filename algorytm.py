fd08887225726c4253221bdd4e62e42da94f9902
zojapila <80410917+zojapila@users.noreply.github.com>
2022-06-07 14:51:39 +0200

    zrobionr może?

--- algorytm.py
+++ algorytm.py
@@ -50,7 +50,7 @@ def wybranie_kroku(matrix, zera, LB):
         if zera[i] > max_val:
             candidate = i
             max_val = zera[i]
-    if max_val != inf:
+    if max_val != inf: #nie działa
         LB += max_val
     matrix[candidate[0]] = [inf for i in range(len(matrix[0]))]
     for i in range(len(matrix)):
@@ -59,44 +59,50 @@ def wybranie_kroku(matrix, zera, LB):
     return LB, matrix, candidate
 
 
-# tab = [[inf, 2, 3, 4, 1, 3, 6, 5, 6, 8],
-#        [9, inf, 4, 3, 8, 3, 5, 1, 6, 3],
-#        [3, 4, inf, 4, 1, 6, 7, 2, 9, 3],
-#        [8, 6, 2, inf, 3, 2, 6, 4, 5, 5],
-#        [7, 4, 5, 3, inf, 7, 8, 3, 4, 6],
-#        [2, 3, 1, 5, 7, inf, 2, 1, 5, 4],
-#        [3, 4, 2, 8, 9, 1, inf, 5, 6, 3],
-#        [8, 4, 5, 6, 1, 2, 3, inf, 6, 1],
-#        [7, 2, 4, 5, 3, 6, 4, 7, inf, 5],
-#        [4, 1, 3, 1, 6, 3, 7, 8, 9, inf]]
-# x, rows, cols = redukcja_macierzy(tab)
-# LB = rows + cols
-# print(np.array(x))
-# print('LB jest równe ', LB)
-# zera = wybor_przejscia(x)
-# print(zera)
+def podproblem(matrix, LB, candidate):
+    matrix2 = matrix.copy()
+    matrix2[candidate[0]][candidate[1]] = inf
+    matrix2, rows, cols = redukcja_macierzy(matrix2)
+    LB2 = LB + rows + cols
+    return matrix2, LB
 
-results = []
+
+matrix = [[inf, 2, 3, 4, 1, 3, 6, 5, 6, 8],
+          [9, inf, 4, 3, 8, 3, 5, 1, 6, 3],
+          [3, 4, inf, 4, 1, 6, 7, 2, 9, 3],
+          [8, 6, 2, inf, 3, 2, 6, 4, 5, 5],
+          [7, 4, 5, 3, inf, 7, 8, 3, 4, 6],
+          [2, 3, 1, 5, 7, inf, 2, 1, 5, 4],
+          [3, 4, 2, 8, 9, 1, inf, 5, 6, 3],
+          [8, 4, 5, 6, 1, 2, 3, inf, 6, 1],
+          [7, 2, 4, 5, 3, 6, 4, 7, inf, 5],
+          [4, 1, 3, 1, 6, 3, 7, 8, 9, inf]]
+
+
+
 LB = 0
-matrix = [[inf, 5, 4, 6, 6],
-          [8, inf, 5, 3, 4],
-          [4, 3, inf, 3, 1],
-          [8, 2, 5, inf, 6],
-          [2, 2, 7, 0, inf]]
-
+# matrix = [[inf, 5, 4, 6, 6],
+#           [8, inf, 5, 3, 4],
+#           [4, 3, inf, 3, 1],
+#           [8, 2, 5, inf, 6],
+#           [2, 2, 7, 0, inf]]
+results = [None for i in range(len(matrix))]
 for i in range(len(matrix[0])):
 
     print(f"\nKROK {i+1}")
     matrix, rows, cols = redukcja_macierzy(matrix)
-    if rows != inf:
-        LB += rows
-    if cols != inf:
-        LB += cols
+    LB += rows + cols
     print('LB jest równe ', LB)
     zera = wybor_przejscia(matrix)
+    LB_pom = LB
     LB, matrix, candidate = wybranie_kroku(matrix, zera, LB)
-    results.append(candidate)
+    LB2, matrix2 = podproblem(matrix, LB_pom, candidate)
+    print("wartiosc lb podproblemu:", LB)
+
+    results[candidate[0]] = (candidate[0]+1, candidate[1]+1)
     print(np.array(matrix))
 
 
 print('\nwyniki:', results)
+
+
