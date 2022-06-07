0f305f603de654e9bd10e493d5477af82ff7e934
zojapila <80410917+zojapila@users.noreply.github.com>
2022-06-07 11:52:17 +0200

    addg

--- algorytm.py
+++ algorytm.py
@@ -20,5 +20,18 @@ def redukcja_macierzy(matrix: list[list], fi=0):
             fi += min_in_cols[i]
             for j in range(len(matrix)):
                 matrix[j][i] -= min_in_cols[i]
-    print(matrix)
-    return matrix, fi
+    return matrix
+
+
+tab = [[inf, 2, 3, 4, 1, 3, 6, 5, 6, 8],
+       [9, inf, 4, 3, 8, 3, 5, 1, 6, 3],
+       [3, 4, inf, 4, 1, 6, 7, 2, 9, 3],
+       [8, 6, 2, inf, 3, 2, 6, 4, 5, 5],
+       [7, 4, 5, 3, inf, 7, 8, 1, 4, 6],
+       [2, 3, 1, 5, 7, inf, 2, 1, 5, 4],
+       [3, 4, 2, 8, 9, 1, inf, 5, 6, 3],
+       [8, 4, 5, 6, 1, 2, 3, inf, 6, 1],
+       [7, 2, 4, 5, 3, 6, 4, 7, inf, 5],
+       [4, 1, 3, 1, 6, 3, 7, 8, 9, inf]]
+
+print(np.array(redukcja_macierzy(tab)))
